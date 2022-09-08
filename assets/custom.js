4324236ec2ba84f1cdc318643b2a654e6116da69
job-allan <adunyaodhiambo@gmail.com>
2022-09-08 17:38:51 +0300

    implemented replies

--- assets/custom.js
+++ assets/custom.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
                                    <h6 class='border-bottom d-inline username'>"+value.user['fullname']+":"+value.cmt['commented_on']+"</h6>\
                                    <p class='para'>"+value.cmt['msg']+"</p>\
                                    <button value="+value.cmt['id']+" class='bage btn-warning reply_btn'>Reply</button>\
-                                   <button class='bage btn-danger view_reply_btn'>View Reply</button>\
+                                   <button value="+value.cmt['id']+" class='bage btn-danger view_reply_btn'>View Reply</button>\
                                    <div class='ml-4 reply_section'>\
                                    </div>\
                                    </div>\
@@ -49,6 +49,62 @@ $(document).ready(function() {
 
         });
 
+        $(document).on('click', '.reply_add_btn', function(){
+          var thisClicked = $(this);
+          var cmt_id = thisClicked.closest('.reply_box').find('.reply_btn').val();
+          var reply = thisClicked.closest('.reply_box').find('.reply_msg').val();
+
+          var data = {
+            comment_id: cmt_id,
+            reply_msg: reply,
+            add_reply: true
+          }
+
+          $.ajax({
+            type: "POST",
+            url: "code.php",
+            data: data,
+            success: function(response){
+              alert(response);
+              $('.reply_section').html("");
+
+            }
+
+          })
+        })
+
+        $(document).on('click', '.view_reply_btn', function(){
+          var thisClicked = $(this);
+          var comment_id = thisClicked.val();
+          $.ajax({
+            type:"POST",
+            url: "code.php",
+            data: {
+              comment_id : comment_id,
+              view_data: true
+            },
+            success: function(response){
+              $('.reply_section').html("");
+              $.each(response, function(key,value){
+
+
+
+              thisClicked.closest('.reply_box').find('.reply_section').html("<div class='sub_reply_box border-bottom p-2 mb-2'>\
+                                   <h6 class='border-bottom d-inline username'>"+value.user['fullname']+"</h6>\
+                                   <p class='para'>"+value.cmt['reply_msg']+":"+value.cmt['comment_on']+"</p>\
+                                   <button value='' class='bage btn-warning sub_reply_btn'>Reply</button>\
+                                   <div class='ml-4 sub_reply_section'>\
+                                   </div>\
+                                   </div>\
+                                   ");
+              });
+
+            }
+
+          });
+
+
+        });
 
 
 
--- code.php
+++ code.php
@@ -3,6 +3,54 @@
 
 
 <?php
+if(isset($_POST['view_data'])){
+  $comment_id = $_POST['comment_id'];
+  $reply_query = "SELECT * FROM comment_replies WHERE comment_id = $comment_id";
+  $reply_result = mysqli_query($connection, $reply_query);
+  $result_array = [];
+
+  if(mysqli_num_rows($reply_result) > 0){
+    foreach ($reply_result as $row) {
+      $user_id = $row['user_id'];
+      $user_query = "SELECT * FROM users WHERE id = $user_id LIMIT 1";
+      $result_user = mysqli_query($connection, $user_query);
+      $user_result = mysqli_fetch_array($result_user);
+
+      array_push($result_array, ['cmt'=>$row, 'user'=>$user_result]);
+
+    }
+    header('Content-Type: application/json');
+    echo json_encode($result_array);
+  }else {
+    echo "give a comment";
+  }
+
+}
+
+
+if(isset($_POST['add_reply'])){
+  $comment_id = $_POST['comment_id'];
+  $reply_msg = $_POST['reply_msg'];
+  $user_id = $_SESSION['auth_user_id'];
+
+  $query = "INSERT INTO comment_replies (user_id, comment_id, reply_msg) ";
+  $query .= "VALUES ($user_id, $comment_id, '$reply_msg')";
+  $result = mysqli_query($connection, $query);
+
+  if($result){
+    echo "replied";
+  }else{
+    echo "Error sending reply".mysqli_error($connection);
+  }
+
+
+
+}
+
+
+
+
+
 if(isset($_POST['comment_load_data'])){
   $comment_query = "SELECT * FROM comments";
   $comment_result = mysqli_query($connection, $comment_query);
